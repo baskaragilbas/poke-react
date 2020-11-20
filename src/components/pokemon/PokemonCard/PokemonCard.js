faee5fd4786357de9c914e479ae171653e15d063
baskaragilbas <baskaragilbas@gmail.com>
2020-11-20 15:41:39 +0700

    moving fetching API to parent component

--- src/components/pokemon/PokemonCard/PokemonCard.js
+++ src/components/pokemon/PokemonCard/PokemonCard.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import styles from './PokemonCard.module.css';
+import {Link} from 'react-router-dom'
 
-
-function PokemonCard({ data }) {
-  const pokeID = data.url.split('/').slice(-2).reverse().pop()
-  const url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokeID + ".png"
-
-  const name = data.name.charAt(0).toUpperCase() + data.name.slice(1)
-
+function PokemonCard({ name, url, pokeID, cardLink }) {
   const [loading, setLoading] = React.useState(true)
-  
   return (
-    <div class="col-md-3 col-sm-6 mb-5">
-      <a class={styles.NoStyle} href={"./" + pokeID}>
+      <Link class={cardLink ? styles.NoStyle : styles.NoLink} to={'../pokemon/'+pokeID}>
         <div class="card">
-          <div class="card-header">
-            {pokeID}
+          <div class="card-header text-left">
+            ID : {pokeID}
           </div>
           {loading ? <div class="text-center">
             <div class="spinner-border" role="status">
@@ -31,8 +24,7 @@ function PokemonCard({ data }) {
             <h5 class="card-title">{name}</h5>
           </div>
         </div>
-      </a>
-    </div>
+      </Link>
   );
 };
 export default PokemonCard;
