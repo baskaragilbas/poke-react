64ae17f59f5ac674ae08b97286d7338138fbc771
baskaragilbas <baskaragilbas@gmail.com>
2020-11-22 23:40:45 +0700

    removing firebase config from public repo

--- src/Firestore.js
+++ src/Firestore.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const  firebaseConfig = {
-    apiKey: "AIzaSyCsfSkxfxJsJ2mY2tWPshZXxbMSQxjDXrc",
-    authDomain: "poke-react-968d2.firebaseapp.com",
-    databaseURL: "https://poke-react-968d2.firebaseio.com",
-    projectId: "poke-react-968d2",
-    storageBucket: "poke-react-968d2.appspot.com",
-    messagingSenderId: "105179896175",
-    appId: "1:105179896175:web:ac9b42aa3705382e0cb09a",
-    measurementId: "G-94S6NB320F"
+    // put config here
 };
 
 firebase.initializeApp(firebaseConfig);
